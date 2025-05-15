7b877a73c4e61cfed5db5875515b5ac3018c2b65
Abdelrahman Mohamed Elshreif <106455463+abdelrahmanelshreif@users.noreply.github.com>
2025-05-15 11:51:38 -0700

    initiating sport league screen

---
+++ Goalzilla/Leagues/view/SportLeagueTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SportLeagueTableViewCell.swift
+//  Goalzilla
+//
+//  Created by Abdelrahman Elshreif on 15/5/25.
+//
+
+import UIKit
+
+class SportLeagueTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Goalzilla/Leagues/view/SportLeaguesDelegete.swift
@@ -0,0 +1,10 @@
+//
+//  SportLeaguesDelegete.swift
+//  Goalzilla
+//
+//  Created by Abdelrahman Elshreif on 15/5/25.
+//
+
+protocol SportLeaguesDelegete{
+    
+}
