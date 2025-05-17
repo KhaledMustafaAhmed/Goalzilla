d2a7dce65490c5545934f8aec4f6518f7666d9d8
Khaled M. Abdelkader <m.abdelkader.khaled@gmail.com>
2025-05-17 22:22:45 +0300

    intial commit for event screen

---
+++ Goalzilla/Events/Presenter/EventScreenPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  EventScreenPresenter.swift
+//  Goalzilla
+//
+//  Created by Khaled Mustafa on 17/05/2025.
+//
+
+protocol EventScreenPresenterProtocol{
+    
+}
+
+class EventScreenPresenter: EventScreenPresenterProtocol{
+    
+}
