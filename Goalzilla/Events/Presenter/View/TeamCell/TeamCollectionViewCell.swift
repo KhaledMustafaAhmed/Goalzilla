55846cb56d8c8917871f2176c0fd376314ebc342
Abdelrahman Mohamed Elshreif <106455463+abdelrahmanelshreif@users.noreply.github.com>
2025-05-22 03:06:50 -0700

    adding background for team cell progrmatically

--- Goalzilla/Events/Presenter/View/TeamCell/TeamCollectionViewCell.swift
+++ Goalzilla/Events/Presenter/View/TeamCell/TeamCollectionViewCell.swift
@@ -13,15 +13,17 @@ class TeamCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var teamImage: UIImageView!
     @IBOutlet weak var teamName: UILabel!
     
+    @IBOutlet weak var mainView: UIView!
     static var resuseIdentifier:String = "TeamCell"
 
-    
+    var backgroundImageView: UIImageView!
     static var nib:UINib{
         return UINib.init(nibName: "TeamCollectionViewCell", bundle: Bundle.init(for: TeamCollectionViewCell.self))
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupBackgroundImage()
         isSkeletonable = true
         contentView.isSkeletonable = true
         teamImage.isSkeletonable = true
@@ -41,5 +43,42 @@ class TeamCollectionViewCell: UICollectionViewCell {
         }else{
             teamImage.image = Utils.placeHolder
         }
+        backgroundImageView.image = UIImage(named: "eventBg1")
+    }
+    
+    func setupBackgroundImage() {
+        backgroundImageView = UIImageView()
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.clipsToBounds = true
+        
+        backgroundImageView.image = UIImage(named: "eventBg1")
+        
+        contentView.insertSubview(backgroundImageView, at: 0)
+        
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        let overlayView = UIView()
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        contentView.insertSubview(overlayView, at: 1)
+        
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            overlayView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            overlayView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            overlayView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            overlayView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        layer.cornerRadius = 20
+        layer.masksToBounds = true
+        
+        contentView.layer.cornerRadius = 20
+        contentView.layer.masksToBounds = true
     }
 }
